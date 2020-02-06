767d701ddafc090c0147190282c82e0b83659f4f
Junwei Luo <junwei.luo9777@gmail.com>
2020-02-06 00:53:42 -0500

    test

---
+++ QuickDBD-export.sql
@@ -0,0 +1,40 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Trainer" (
+    "ID" int   NOT NULL,
+    "FName" varchar   NOT NULL,
+    "LName" varchar   NOT NULL,
+    "Gym_ID" int   NOT NULL,
+    CONSTRAINT "pk_Trainer" PRIMARY KEY (
+        "ID"
+     )
+);
+
+CREATE TABLE "members" (
+    "ID" int   NOT NULL,
+    "FName" varchar   NOT NULL,
+    "LName" varchar   NOT NULL,
+    "Address" varchar   NOT NULL,
+    "Trainer" varchar   NOT NULL,
+    "gym_ID" int   NOT NULL,
+    CONSTRAINT "pk_members" PRIMARY KEY (
+        "ID"
+     )
+);
+
+CREATE TABLE "gym" (
+    "ID" int   NOT NULL,
+    "Address" varchar   NOT NULL,
+    CONSTRAINT "pk_gym" PRIMARY KEY (
+        "ID"
+     ),
+    CONSTRAINT "uc_gym_Address" UNIQUE (
+        "Address"
+    )
+);
+
+ALTER TABLE "members" ADD CONSTRAINT "fk_members_gym_ID" FOREIGN KEY("gym_ID")
+REFERENCES "gym" ("ID");
+
